421fc2fff753e685ecdc7a322c80edd8257a174f
Dell_E6540 <yaroslav.basalyk@gmail.com>
2018-08-01 17:34:41 +0300

    upp filex

--- calculator/index.js
+++ calculator/index.js
@@ -1,15 +1,15 @@
 //autoload functions
 document.addEventListener('DOMContentLoaded', function() {
-  classbutton();
+  classButton();
   });
   
-  function classbutton() {
+  function classButton() {
   //variables
   var numbers = document.querySelectorAll('.number');
   operations = document.querySelectorAll('.operations');
-  decimal_btn = document.getElementById('decimal_btn');
-  clear_btn = document.getElementById('clear_btn');
-  equally_btn = document.getElementById('equally_btn');
+  decimalBtn = document.getElementById('decimalBtn');
+  clearBtn = document.getElementById('clearBtn');
+  equallyBtn = document.getElementById('equallyBtn');
   display = document.getElementById('display');
   //memoryCurrentNumber = 0;
   //memoryNewNumber = false;
@@ -18,54 +18,54 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < numbers.length; i++) {
     number = numbers[i];
     number.addEventListener('click', function(e) {
-    numbers_func(e.target.textContent);
+    numbersFunc(e.target.textContent);
     });
   }
   
   for (var i = 0; i < operations.length; i++) {
-    operation_btn = operations[i];
-    operation_btn.addEventListener('click', function(e) {
-    operation_func(e.target.textContent);
+    operationBtn = operations[i];
+    operationBtn.addEventListener('click', function(e) {
+    operationFunc(e.target.textContent);
     });
   }
   
-  decimal_btn.addEventListener('click', decimal_func);
-  clear_btn.addEventListener('click', clear_func);
-  equally_btn.addEventListener('click', equally_func);
+  decimalBtn.addEventListener('click', decimalFunc);
+  clearBtn.addEventListener('click', clearFunc);
+  equallyBtn.addEventListener('click', equallyFunc);
   
   //functions
-  function numbers_func(numbers) {
-    var display_result;
-    display_result = document.getElementById('display_result');
-    display_result.innerHTML = numbers;
+  function numbersFunc(numbers) {
+    var displayResult;
+    displayResult = document.getElementById('display-result');
+    displayResult.innerHTML = numbers;
     console.log('Click on button ' + numbers + '');
   }
   
-  function operation_func(operations) {
-    var display_result;
-    display_result = document.getElementById('display_result');
-    display_result.innerHTML = operations;
+  function operationFunc(operations) {
+    var displayResult;
+    displayResult = document.getElementById('display-result');
+    displayResult.innerHTML = operations;
     console.log('Click on button ' + operations + '');
   }
   
-  function clear_func(clear_btn) {
-    var display_result;
-    display_result = document.getElementById('display_result');
-    display_result.innerHTML = 'C';
-    console.log('Click on button ' + clear_btn + '');
+  function clearFunc(clearBtn) {
+    var displayResult;
+    displayResult = document.getElementById('display-result');
+    displayResult.innerHTML = 'C';
+    console.log('Click on button ' + clearBtn + '');
   }
   
-  function equally_func(equally_btn) {
-    var display_result;
-    display_result = document.getElementById('display_result');
-    display_result.innerHTML = '=';
-    console.log('Click on button ' + equally_btn + '');
+  function equallyFunc(equallyBtn) {
+    var displayResult;
+    displayResult = document.getElementById('display-result');
+    displayResult.innerHTML = '=';
+    console.log('Click on button ' + equallyBtn + '');
   }
   
-  function decimal_func(decimal_btn) {
-    var display_result;
-    display_result = document.getElementById('display_result');
-    display_result.innerHTML = '.';
-    console.log('Click on button ' + decimal_btn + '');
+  function decimalFunc(decimalBtn) {
+    var displayResult;
+    displayResult = document.getElementById('display-result');
+    displayResult.innerHTML = '.';
+    console.log('Click on button ' + decimalBtn + '');
   }
 }
