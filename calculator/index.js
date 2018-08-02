a5faa619e64b2e8e216147fad3af674dca704d1d
Poliakov Maksym <polmax09@gmail.com>
2018-08-02 09:42:06 +0300

    [Refactor] Renamed HTML element id using kebab-case

--- calculator/index.js
+++ calculator/index.js
@@ -2,37 +2,37 @@
 document.addEventListener('DOMContentLoaded', function() {
   classButton();
   });
-  
+
   function classButton() {
   //variables
   var numbers = document.querySelectorAll('.number');
   operations = document.querySelectorAll('.operations');
-  decimalBtn = document.getElementById('decimalBtn');
-  clearBtn = document.getElementById('clearBtn');
-  equallyBtn = document.getElementById('equallyBtn');
+  decimalBtn = document.getElementById('decimal-btn');
+  clearBtn = document.getElementById('clear-btn');
+  equallyBtn = document.getElementById('equally-btn');
   display = document.getElementById('display');
   //memoryCurrentNumber = 0;
   //memoryNewNumber = false;
   //memoryPendingOperation = '';
-  
+
   for (var i = 0; i < numbers.length; i++) {
     number = numbers[i];
     number.addEventListener('click', function(e) {
     numbersFunc(e.target.textContent);
     });
   }
-  
+
   for (var i = 0; i < operations.length; i++) {
     operationBtn = operations[i];
     operationBtn.addEventListener('click', function(e) {
     operationFunc(e.target.textContent);
     });
   }
-  
+
   decimalBtn.addEventListener('click', decimalFunc);
   clearBtn.addEventListener('click', clearFunc);
   equallyBtn.addEventListener('click', equallyFunc);
-  
+
   //functions
   function numbersFunc(numbers) {
     var displayResult;
@@ -40,28 +40,28 @@ document.addEventListener('DOMContentLoaded', function() {
     displayResult.innerHTML = numbers;
     console.log('Click on button ' + numbers + '');
   }
-  
+
   function operationFunc(operations) {
     var displayResult;
     displayResult = document.getElementById('display-result');
     displayResult.innerHTML = operations;
     console.log('Click on button ' + operations + '');
   }
-  
+
   function clearFunc(clearBtn) {
     var displayResult;
     displayResult = document.getElementById('display-result');
     displayResult.innerHTML = 'C';
     console.log('Click on button ' + clearBtn + '');
   }
-  
+
   function equallyFunc(equallyBtn) {
     var displayResult;
     displayResult = document.getElementById('display-result');
     displayResult.innerHTML = '=';
     console.log('Click on button ' + equallyBtn + '');
   }
-  
+
   function decimalFunc(decimalBtn) {
     var displayResult;
     displayResult = document.getElementById('display-result');
